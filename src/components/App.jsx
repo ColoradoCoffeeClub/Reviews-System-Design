894443aae9c73090b2dacff3b5d3eaa7f5d959bc
Utkarsh Singh <singh.ut7@gmail.com>
2020-09-08 18:01:37 -0600

    Refactor front-end to work with API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,66 @@
-import React from 'react';
-import Ratings from './Ratings.jsx';
-import Reviews from './Reviews.jsx';
-import AddReview from './AddReview.jsx';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import Ratings from "./Ratings.jsx";
+import Reviews from "./Reviews.jsx";
+import AddReview from "./AddReview.jsx";
+import { Grid } from "@material-ui/core";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      product_id: 24,// Math.floor(Math.random() * 100), //21
+      product_id: 24, // Math.floor(Math.random() * 100), //21
       meta: {},
       reviews: {},
       modalIsOpen: false,
-      sort: 'newest',
-      filterBy: undefined
-    }
+      sort: "newest",
+      filterBy: undefined,
+    };
   }
 
   fetchMetadata() {
     fetch(`/reviews/${this.state.product_id}/meta`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         this.setState({
-          meta: data
+          meta: data,
         });
       });
   }
 
-  fetchReviews(sortBy = '') {
-    let params = '?count=100';
+  fetchReviews(sortBy = "") {
+    const url = `/reviews/${this.state.product_id}/list`;
 
-    if(sortBy !== '') params += `&sort=${sortBy}`;
-
-    const url = `/reviews/${this.state.product_id}/list${params}`;
-
-    console.log(url)
+    console.log(url);
 
     fetch(url)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         this.setState({
-          reviews: data
+          reviews: data,
         });
       });
   }
 
   componentDidMount() {
     this.fetchMetadata();
-    this.fetchReviews('newest');
+    this.fetchReviews("newest");
   }
 
   addReview() {
     this.setState({
-      modalIsOpen: true
+      modalIsOpen: true,
     });
   }
 
   closeModal() {
     this.setState({
-      modalIsOpen: false
+      modalIsOpen: false,
     });
   }
 
   setSort(value) {
-    this.setState({sort: value});
+    this.setState({ sort: value });
     this.fetchReviews(value);
   }
 
@@ -90,9 +86,14 @@ class App extends React.Component {
             filterBy={this.state.filterBy}
           />
         </Grid>
-        <AddReview isOpen={this.state.modalIsOpen} closeModal={this.closeModal.bind(this)} meta={this.state.meta} update={this.fetchReviews.bind(this)} />
+        {/* <AddReview
+          isOpen={this.state.modalIsOpen}
+          closeModal={this.closeModal.bind(this)}
+          meta={this.state.meta}
+          update={this.fetchReviews.bind(this)}
+        /> */}
       </div>
-    )
+    );
   }
 }
 
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Grid, Typography, Link, Button, Select, MenuItem, InputLabel, FormControl } from '@material-ui/core';
-import { Rating } from '@material-ui/lab';
-import moment from 'moment';
+import React from "react";
+import {
+  Grid,
+  Typography,
+  Link,
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import moment from "moment";
 
 class Reviews extends React.Component {
   constructor(props) {
@@ -12,184 +21,184 @@ class Reviews extends React.Component {
       markedHelpful: {},
       reported: {},
       modalIsOpen: false,
-      showMore: {}
+      showMore: {},
     };
   }
 
   render() {
-    return(
-      <Grid id="reviews" container item xs={9}>
-        { this.renderSort() }
-        { this.renderReviews() }
-      </Grid>
-    )
-  }
-
-  renderSort() {
-    const sortOptions = ['newest', 'helpful', 'relevant'];
-
-    const { sort } = this.props;
-
-    const totalReviews = Object.keys(this.props.reviews).length === 0 ? 0 : this.props.reviews.results.length;
-
-    const sortBy = this.props.sort === 'relevant' ? 'relevance' : this.props.sort;
-
     return (
-      <div id="reviews-sort">
-        <Typography id="sort-menu-title" variant="h6">{totalReviews} {totalReviews === 1 ? 'review' : 'reviews'}, sorted by {sortBy}</Typography>
-        <InputLabel id="sort-menu-label">Sort By</InputLabel>
-        <Select
-          labelId="sort-menu-label"
-          id="sort-menu"
-          value={sort || ''}
-          onChange={e => this.props.setSort(e.target.value)}
-        >
-          { sortOptions.map((sortBy, index) => <MenuItem key={index} value={sortBy}>{sortBy}</MenuItem>) }
-        </Select>
-      </div>
-    )
-
+      <Grid id="reviews" container item xs={9}>
+        {this.renderReviews()}
+      </Grid>
+    );
   }
 
   renderReviews() {
     const ready = Object.keys(this.props.reviews).length;
-    if(!ready) return;
+    if (!ready) return;
 
     let { results } = this.props.reviews ? this.props.reviews : null;
 
-    if(this.props.filterBy) results = results.filter(review => review.rating === this.props.filterBy);
-
-    const reviews = results.slice(0, this.state.showReviews);
+    const reviews = results;
 
     return (
       <Grid container item id="reviews-list" direction="column">
-        { reviews.map((review, index) => {
-          const { reviewer_name, rating, summary, body, recommend, date, helpfulness, photos, response, review_id } = review;
-          return(
-              <Grid key={index} className="user-review" container item direction="column">
-                <Grid container item justify="space-between">
-                  <Grid item>
-                    <Rating
-                      value={rating}
-                      precision={0.25}
-                      size="small"
-                      readOnly
-                    />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant='body2'>
-                      {reviewer_name}, {moment(date).format('MMMM Do YYYY')}
-                    </Typography>
-                  </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant='h4' noWrap>{summary}</Typography>
+        {this.props.reviews.map((review, index) => {
+          const {
+            reviewer_name,
+            rating,
+            summary,
+            body,
+            recommend,
+            date,
+            helpfulness,
+            photos,
+            response,
+            review_id,
+          } = review;
+          return (
+            <Grid
+              key={index}
+              className="user-review"
+              container
+              item
+              direction="column"
+            >
+              <Grid container item justify="space-between">
+                <Grid item>
+                  <Rating
+                    value={rating}
+                    precision={0.25}
+                    size="small"
+                    readOnly
+                  />
                 </Grid>
                 <Grid item>
-                  {
-                    body.length > 250
-                    ?
-                    <Typography variant="body1" onClick={ () => this.toggleShowMore(review_id)}>
-                      {
-                        this.state.showMore[review_id]
-                        ?
-                        body
-                        :
-                        body.slice(0, 250) + '...show more'
-                      }
-                    </Typography>
-                    :
-                    <Typography variant="body1">{body}</Typography>
-                  }
-                </Grid>
-                {
-                  recommend
-                  ?
-                  <Grid item>
-                    <Typography variant='body2'>I recommend this product</Typography>
-                  </Grid>
-                  : null
-                }
-                {
-                  (response && response !== 'null' && response !== '')
-                  ?
-                  <Grid className="response"  item>
-                    <Typography variant="h6">Response:</Typography>
-                    <Typography variant="body1">{response}</Typography>
-                  </Grid>
-                  : null
-                }
-                {
-                  photos.length
-                  ?
-                  <div id="reviews-photos-container">
-                    <div>Photos:</div>
-                    <div id="reviews-photos">
-                      {
-                      photos.map(({url}, index) => <img src={url} key={index} />)
-                      }
-                    </div>
-                  </div>
-                  : null
-                }
-                <Grid className="review-footer" item>
                   <Typography variant="body2">
-                    Helpful? <Link underline="always" onClick={() => this.markHelpful(review_id)}>Yes</Link>
-                    <span> ({helpfulness}) </span>
-                    <Link underline="always" onClick={() => this.reportReview(review_id)}>Report</Link>
+                    {reviewer_name}, {moment(date).format("MMMM Do YYYY")}
                   </Typography>
                 </Grid>
               </Grid>
-          )
-        }) }
+              <Grid item xs={12}>
+                <Typography variant="h4" noWrap>
+                  {summary}
+                </Typography>
+              </Grid>
+              <Grid item>
+                {body.length > 250 ? (
+                  <Typography
+                    variant="body1"
+                    onClick={() => this.toggleShowMore(review_id)}
+                  >
+                    {this.state.showMore[review_id]
+                      ? body
+                      : body.slice(0, 250) + "...show more"}
+                  </Typography>
+                ) : (
+                  <Typography variant="body1">{body}</Typography>
+                )}
+              </Grid>
+              {recommend ? (
+                <Grid item>
+                  <Typography variant="body2">
+                    I recommend this product
+                  </Typography>
+                </Grid>
+              ) : null}
+              {response && response !== "null" && response !== "" ? (
+                <Grid className="response" item>
+                  <Typography variant="h6">Response:</Typography>
+                  <Typography variant="body1">{response}</Typography>
+                </Grid>
+              ) : null}
+              {photos.length ? (
+                <div id="reviews-photos-container">
+                  <div>Photos:</div>
+                  <div id="reviews-photos">
+                    {photos.map(({ url }, index) => (
+                      <img src={url} key={index} />
+                    ))}
+                  </div>
+                </div>
+              ) : null}
+              <Grid className="review-footer" item>
+                <Typography variant="body2">
+                  Helpful?{" "}
+                  <Link
+                    underline="always"
+                    onClick={() => this.markHelpful(review_id)}
+                  >
+                    Yes
+                  </Link>
+                  <span> ({helpfulness}) </span>
+                  <Link
+                    underline="always"
+                    onClick={() => this.reportReview(review_id)}
+                  >
+                    Report
+                  </Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          );
+        })}
         <Grid id="reviews-buttons" container item>
-          {
-          results.length > this.state.showReviews
-          ?
-            <Button id="more-reviews" xs={4} disableElevation variant="outlined" onClick={this.loadMoreReviews.bind(this)}>
+          {this.props.reviews.length > this.state.showReviews ? (
+            <Button
+              id="more-reviews"
+              xs={4}
+              disableElevation
+              variant="outlined"
+              onClick={this.loadMoreReviews.bind(this)}
+            >
               More Reviews
             </Button>
-          : null
-        }
-          <Button id="add-review" xs={4} disableElevation variant="outlined" onClick={this.props.addReview}>
+          ) : null}
+          <Button
+            id="add-review"
+            xs={4}
+            disableElevation
+            variant="outlined"
+            onClick={this.props.addReview}
+          >
             + Add Review
           </Button>
         </Grid>
       </Grid>
-    )
-
+    );
   }
 
   loadMoreReviews() {
     const newReviews = this.state.showReviews + 2;
     this.setState({
-      showReviews: newReviews
+      showReviews: newReviews,
     });
   }
 
   markHelpful(review_id) {
     if (this.state.markedHelpful[review_id] === true) return;
-    fetch(`/reviews/helpful/${review_id}/`, { method: 'PUT' })
-      .then(() => {
-        this.props.update();
-        this.setState({ markedHelpful: {...this.state.markedHelpful, [review_id]: true} })
+    fetch(`/reviews/helpful/${review_id}/`, { method: "PUT" }).then(() => {
+      this.props.update();
+      this.setState({
+        markedHelpful: { ...this.state.markedHelpful, [review_id]: true },
       });
+    });
   }
 
   reportReview(review_id) {
-    fetch(`/reviews/report/${review_id}/`, { method: 'PUT' })
-      .then(() => this.props.update())
+    fetch(`/reviews/report/${review_id}/`, { method: "PUT" }).then(() =>
+      this.props.update()
+    );
   }
 
   toggleShowMore(review_id) {
     this.setState({
       showMore: {
         ...this.state.showMore,
-        [review_id]: !this.state.showMore[review_id]
-      }
-    })
+        [review_id]: !this.state.showMore[review_id],
+      },
+    });
   }
-
 }
 
 export default Reviews;
